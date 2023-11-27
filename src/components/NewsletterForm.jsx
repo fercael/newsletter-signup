7d2ba80d18b8e34be8d0f3cbcac008ca80c2b935
carlos fernandes <carlosfernandes6@outlook.com>
2023-11-27 15:57:13 -0300

    chore(NewsletterForm): Adicionar os icones da lista.

--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -2,6 +2,7 @@ import styles from "./NewsletterForm.module.css"
 
 import illustrationDesktop from "../assets/images/illustration-sign-up-desktop.svg"
 import illustrationMobile from "../assets/images/illustration-sign-up-mobile.svg"
+import iconList from "../assets/images/icon-list.svg"
 
 function NewsletterForm() {
     return(
@@ -15,9 +16,18 @@ function NewsletterForm() {
                 <h1 className={styles.NewsletterFormH1}>Stay updated!</h1>
                 <p className={styles.NewsletterFormP}>Join 60,000+ product managers receiving monthly updates on:</p>
                 <ul className={styles.NewsletterFormUl}>
-                    <li className={styles.NewsletterFormLi}>Product discovery and building what matters</li>
-                    <li className={styles.NewsletterFormLi}>Measuring to ensure updates are a success</li>
-                    <li className={styles.NewsletterFormLi}>And much more!</li>
+                    <li className={styles.NewsletterFormLi}> 
+                        <img src={iconList} />
+                        <p className={styles.NewsletterFormP}>Product discovery and building what matters</p>
+                    </li>
+                    <li className={styles.NewsletterFormLi}>
+                        <img src={iconList} />
+                        <p className={styles.NewsletterFormP}>Measuring to ensure updates are a success</p>
+                    </li>
+                    <li className={styles.NewsletterFormLi}>
+                        <img src={iconList} />
+                        <p className={styles.NewsletterFormP}>And much more!</p>
+                    </li>
                 </ul>
             </header>
             <form className={styles.NewsletterFormForm} action="">
